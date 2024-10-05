1cc7753a4b7aefb29d10cd82e1221b17fb1e65c4
Gabriel Garcia <garciacaysahuana@gmail.com>
2024-10-04 22:09:13 -0500

    files rename and add file

---
+++ 11_3_web_scraping_exercise_01.py
@@ -0,0 +1,13 @@
+'''Instalación y Configuración de requests: Instala la librería requests en 
+tu entorno de trabajo y realiza una simple solicitud HTTP GET a una página 
+web de prueba para verificar que la instalación fue exitosa.'''
+
+import requests 
+path = "https://books.toscrape.com/index.html"
+
+
+response = requests.get(path)
+print(response.status_code)
+print(response.text)
+
+
