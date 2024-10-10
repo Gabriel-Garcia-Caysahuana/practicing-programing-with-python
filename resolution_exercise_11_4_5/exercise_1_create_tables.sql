80b2405ab51e66a826f89ab19869042392c8219c
Gabriel Garcia <garciacaysahuana@gmail.com>
2024-10-10 10:31:21 -0500

    modified exercise_1_create_tables.sql, and added model_relational.mwb and model_relational.png

--- resolution_exercise_11_4_5/exercise_1_create_tables.sql
+++ resolution_exercise_11_4_5/exercise_1_create_tables.sql
@@ -1,38 +1,50 @@
+USE exercise_11_4_5;
+
 CREATE TABLE Usuarios (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT NOT NULL,
-    correo TEXT NOT NULL UNIQUE,
-    fecha_registro DATE
-) CREATE TABLE Categorias (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre_categoria TEXT NOT NULL,
-    descripcion TEXT
-) CREATE TABLE Productos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre_producto TEXT NOT NULL,
-    descripcion TEXT,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL,
+    correo VARCHAR(255) NOT NULL UNIQUE,
+    fecha_registro DATE 
+);
+
+CREATE TABLE Categorias (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nombre_categoria VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255)
+);
+
+CREATE TABLE Productos (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nombre_producto VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255),
     precio INTEGER NOT NULL,
     cantidad_stock INTEGER NOT NULL,
     id_categoria INTEGER NOT NULL,
-    Foreign key (id_categoria) REFERENCES Categorias(id)
-) CREATE TABLE Ordenes(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    FOREIGN KEY (id_categoria) REFERENCES Categorias(id)
+);
+
+CREATE TABLE Ordenes (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     id_cliente INTEGER NOT NULL,
-    fecha_orden DATE NOT NULL,
+    fecha_orden DATE NOT NULL, 
     total_compra INTEGER NOT NULL,
-    Foreign key (id_cliente) REFERENCES Usuarios(id)
-) CREATE TABLE OrdenProductos (
+    FOREIGN KEY (id_cliente) REFERENCES Usuarios(id)
+);
+
+CREATE TABLE OrdenProductos (
     id_orden INTEGER NOT NULL,
     id_producto INTEGER NOT NULL,
-    catidad_comprada INTEGER NOT NULL,
+    cantidad_comprada INTEGER NOT NULL,
     FOREIGN KEY (id_orden) REFERENCES Ordenes(id),
     FOREIGN KEY (id_producto) REFERENCES Productos(id)
-) CREATE TABLE Comentarios (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+);
+
+CREATE TABLE Comentarios (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     id_producto INTEGER NOT NULL,
     id_usuario INTEGER NOT NULL,
-    texto_comentario TEXT,
+    texto_comentario VARCHAR(255),
     fecha_comentario DATE NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES Usuarios(id),
     FOREIGN KEY (id_producto) REFERENCES Productos(id)
-)
+);
